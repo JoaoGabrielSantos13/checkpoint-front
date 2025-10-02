c5a879a36968eae13d8282eace58f3819cb41ace
Leonardo Vinícius <leonardo.v.souzafilho@gmail.com>
2025-10-02 11:23:30 -0300

    alinhamento dos cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ title, description}) {
     return(
         <>
-            <div className="bg-white border rounded-x1 shadow-md p-6 hover:shadow-lg duration-300 rounded-2xl transition-shadow flex flex-col gap-2">
+            <div className="bg-white border rounded-x1 shadow-md p-6 hover:shadow-lg duration-300 rounded-2xl transition-shadow flex flex-col gap-2 items-center justify-between">
                 <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold">{title}</h2>
                 <p className="text-gray-600 lg:text-[18px]">{description}</p>
             </div>
